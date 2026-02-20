2c236ff8a1aa9dbad3215e3b80e4bf33c50c7726
Paridhi-Bhardwaj <bhardwajparidhi14@gmail.com>
2026-02-20 19:16:34 +0530

    Fix PrismaClient import for Prisma 7

--- server/lib/prisma.ts
+++ server/lib/prisma.ts
@@ -1,10 +1,15 @@
 import "dotenv/config";
-import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from "@prisma/client/extension";
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const connectionString = `${process.env.DATABASE_URL}`
+const connectionString = process.env.DATABASE_URL!;
 
-const adapter = new PrismaPg({ connectionString })
-const prisma = new PrismaClient({ adapter })
+const adapter = new PrismaPg({
+  connectionString,
+});
 
-export default prisma 
+const prisma = new PrismaClient({
+  adapter,
+});
+
+export default prisma;
