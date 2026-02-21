8803a5556ea633ee0055c3bf3142b706d7b152dc
Paridhi-Bhardwaj <bhardwajparidhi14@gmail.com>
2026-02-21 18:01:24 +0530

    Fix Better Auth baseURL env variable

--- client/src/lib/auth-client.ts
+++ client/src/lib/auth-client.ts
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
-    baseURL: import.meta.env.VITE_BASEURL ,
+    baseURL: import.meta.env.VITE_BASE_URL ,
     fetchOptions:{ credentials: 'include'},
 })
 
